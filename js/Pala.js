15df130d33eccc9466f01d444268e6e8cfd4fb03
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2015-08-19 18:32:57 -0500

    Create Pala.js

---
+++ js/Pala.js
@@ -0,0 +1,29 @@
+/**********************************/
+/*       Pablo Iván G. Soto       */
+/*             2015               */
+/**********************************/
+function Pala(x, mundoAlto) {
+  this.mundoAlto = mundoAlto;
+  this.ancho = 20;
+  this.alto = 80;
+  this.pos = new Vector2D(x, mundoAlto / 2);
+  this.velocidad = 220;
+}
+Pala.prototype.mover = function (delta, direccion) {
+  var distancia = Math.round(delta * this.velocidad);
+  switch (direccion)
+    {
+    case Direccion.ARRIBA:
+      this.pos.y -= distancia;
+      this.pos.y = Math.max(this.alto / 2, this.pos.y);
+      break;
+    case Direccion.ABAJO:
+      this.pos.y += distancia;
+      this.pos.y = Math.min(this.mundoAlto - this.alto / 2, this.pos.y);
+      break;
+  }
+};
+Pala.prototype.dibujar = function (contexto) {
+  contexto.fillStyle = 'blue';
+  contexto.fillRect(this.pos.x - this.ancho / 2, this.pos.y - this.alto / 2, this.ancho, this.alto);
+};
