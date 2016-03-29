b5d712301c73bc8134f6757c70a346bf7e1af59e
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2016-03-29 17:28:46 -0600

    Updated angle.

--- js/Pelota.js
+++ js/Pelota.js
@@ -12,7 +12,7 @@ function Pelota(mundoAncho, mundoAlto, pong) {
   this.velocidad = 300;
   this.velocidadIni = 300;
   this.incrementoVelocidad = 10;
-  this.maxAngulo = 60;
+  this.maxAngulo = 75;
   this.velocidad = this.velocidadIni;
   this.vector;
   this.sacar( - 1);
