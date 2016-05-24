07adac1ed629d0512c11b51d5d2fff1bad34b78d
Pablo Iván G. Soto <pabloivangomezsoto@gmail.com>
2016-05-24 14:49:41 -0500

    Updated speed.

--- js/Pala.js
+++ js/Pala.js
@@ -7,7 +7,7 @@ function Pala(x, mundoAlto) {
   this.ancho = 20;
   this.alto = 90;
   this.pos = new Vector2D(x, mundoAlto / 2);
-  this.velocidad = 310;
+  this.velocidad = 900;
 }
 Pala.prototype.mover = function (delta, direccion) {
   var distancia = Math.round(delta * this.velocidad);
